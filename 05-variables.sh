63676626b386bfdf0adbd08568b949ea1d9e9b7d
Sunil <sunilbujji373@gmail.com>
2024-12-11 23:45:04 +0530

    shell scripts

--- 05-variables.sh
+++ 05-variables.sh
@@ -7,3 +7,8 @@ echo "Please enter your Password:: "
 read -s PASSWORD
 
 echo "Username is: $USERNAME, Password is: $PASSWORD" # I am priting just for validation, you should not print username and passwords in scripts
+
+
+echo "Enter secret input:"
+read -s secret_input
+echo "You entered something $secret_input"
