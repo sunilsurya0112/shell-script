047e9b3d09980515a573c1eaae659b42abdea602
Sunil <sunilbujji373@gmail.com>
2024-12-14 22:45:15 +0530

    shell script

---
+++ 11.logs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+ID=$(id -u)
+
+echo "script name is: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then    
+        echo "$2 is failed"
+        exit 1 #other than zero
+    else
+        echo "$1 is Success"
+    fi     
+
+}
+
+
+if [ $1 -ne 0 ]
+then
+    echo "you are not root user"
+    exit 34
+else
+    echo "you are root user"    
+fi # fi means reverse of if , indicating condition end
+
+apt install mysql-server -y
+
+VALIDATE $? "Mysql installation"
+
+apt install git -y
+
+VALIDATE $? "git installation"
+
