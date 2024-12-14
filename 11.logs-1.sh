cda36ccfad404c5414604d75c27d1f847d1ef423
Sunil <sunilbujji373@gmail.com>
2024-12-15 00:48:00 +0530

    shell script

--- 11.logs-1.sh
+++ 11.logs-1.sh
@@ -28,7 +28,7 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-apt install mysql-server -y >> $LOGFILE
+apt install mysql-server -y &>> $LOGFILE
 
 VALIDATE $? "Installing  MySQL"
 
