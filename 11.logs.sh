3f78dbb18769293d52db468dbd2f716344f4657a
Sunil <sunilbujji373@gmail.com>
2024-12-15 11:49:50 +0530

    shell script

--- 11.logs.sh
+++ 11.logs.sh
@@ -15,7 +15,7 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then    
         echo "$2 is $R failed $N"
-        exit 1 #other than zero
+        #exit 1 #other than zero
     else
         echo "$2 is $G Success $N"
     fi     
@@ -26,7 +26,7 @@ VALIDATE(){
 if [ $ID -ne 0 ]
 then
     echo "you are not root user"
-    #exit 34
+    exit 34
 else
     echo "you are root user"    
 fi # fi means reverse of if , indicating condition end
