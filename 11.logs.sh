1883f7d89935f3eb2ee7c74a2ae3e9db52a30fc5
Sunil <sunilbujji373@gmail.com>
2024-12-15 15:37:54 +0530

    shell script

--- 11.logs.sh
+++ 11.logs.sh
@@ -13,10 +13,10 @@ echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo  "ERROR:: $2 ... $R FAILED $N"
+        echo -e "ERROR:: $2 ... $R FAILED $N"
         exit 1
     else
-        echo  "$2 ... $G SUCCESS $N"
+        echo  -e "$2 ... $G SUCCESS $N"
     fi
 }
 
